dfa5066e62451001b72f747535371419ac18161a
ahung93 <ahung93@gmail.com>
2017-01-21 14:02:11 -0800

    Animation Controller for Ninja so that there is a short delay before each attack.
    - In the middle of scripting the attack behavior of the ninja

---
+++ Assets/GGJ2017/Scripts/PlayerBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+    public int health = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage(int damage)
+    {
+
+    }
+}
